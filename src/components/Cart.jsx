9ffeba861e61309845235ac51428cccc7a042f6e
Bruno123-n <hungriakaa@gmail.com>
2026-07-05 12:14:23 -0300

    implement shopping cart with total"

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,12 @@
+import formatPrice from "../utils/formatPrice"
+
+
 function Cart({ carrinho }) {
+
+    const total = carrinho.reduce((acumulador, produto) => {
+        return acumulador + produto.preco
+    },0)
+
     if (carrinho.length === 0) {
         return ( 
             <div className="cart">
@@ -12,12 +20,22 @@ function Cart({ carrinho }) {
             <h2>🛒 Carrinho</h2>
 
             {carrinho.map((produto) => (
-            <p key={produto.id}
-            >
-                {produto.nome}
-            </p>
+                <div key={produto.id}>
+
+                    <p>
+                        {produto.nome}
+                    </p>
+                    <p>
+                        {formatPrice(produto.preco)}
+                    </p>
+                    <p>
+                        Total: {formatPrice(total)}
+                    </p>
+
+                </div>
             
             ))}
+
         </div>
     )
 }
