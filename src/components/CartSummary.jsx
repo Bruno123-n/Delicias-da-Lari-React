6b3290df5e75cb6dc4758aa9842c9dc740308df2
Bruno123-n <hungriakaa@gmail.com>
2026-07-23 08:59:12 -0300

    style(cart-summary): improve visual hierarchy and total price highlight

--- src/components/CartSummary.jsx
+++ src/components/CartSummary.jsx
@@ -1,13 +1,20 @@
-import formatPrice from "../utils/formatPrice"
+import formatPrice from "../utils/formatPrice";
+import "./CartSummary.css";
 
+function CartSummary({ total, totalItens }) {
+  return (
+    <div className="summary">
+      <div className="summary-item">
+        <span>Total de itens:</span>
+        <span className="valor-itens">{totalItens}</span>
+      </div>
 
-function CartSummary({total, totalItens}){
-    return(
-        <>
-            <p>Total: {formatPrice(total)}</p>
-            <p>Total de itens: {totalItens}</p>
-        </>
-    )
+      <div className="summary-item total-destaque">
+        <span>Total:</span>
+        <span className="valor-total">{formatPrice(total)}</span>
+      </div>
+    </div>
+  );
 }
 
-export default CartSummary
+export default CartSummary;
